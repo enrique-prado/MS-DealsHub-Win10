4f0af692ec63abc6ba5efcca6d5aef1c4b1a5c0b
Enrique Prado <eprado@weareratio.com>
2015-08-05 21:34:59 -0700

    Basic landing page done using split view

--- MS-DealsHub-Win10/MainPage.xaml.cs
+++ MS-DealsHub-Win10/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -26,5 +27,36 @@ namespace MS_DealsHub_Win10
         {
             this.InitializeComponent();
         }
+
+        private ObservableCollection<NavLink> _navLinks = new ObservableCollection<NavLink>()
+            {
+                new NavLink() { Label = "Featured", Symbol = Windows.UI.Xaml.Controls.Symbol.Favorite  },
+                new NavLink() { Label = "Apps", Symbol = Windows.UI.Xaml.Controls.Symbol.AllApps },
+                new NavLink() { Label = "Games", Symbol = Windows.UI.Xaml.Controls.Symbol.People },
+                new NavLink() { Label = "Music", Symbol = Windows.UI.Xaml.Controls.Symbol.MusicInfo },
+                new NavLink() { Label = "TV & Movies", Symbol = Windows.UI.Xaml.Controls.Symbol.Video }
+            };
+
+        public ObservableCollection<NavLink> NavLinks
+        {
+            get { return _navLinks; }
+        }
+
+        private void NavLinksList_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            content.Text = (e.ClickedItem as NavLink).Label + " Page";
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+        }
+
+    }
+
+    public class NavLink
+    {
+        public string Label { get; set; }
+        public Symbol Symbol { get; set; }
     }
 }
